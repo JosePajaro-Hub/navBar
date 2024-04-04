70dc8e526b99dc5ceb333c6fddb036ad7fe0d253
JosePajaro-Hub <jose.pajaro@gse.com.co>
2024-04-04 08:22:05 -0500

    InitialCommit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter} from "react-router-dom"
+import "./index.css";
+import NavrbarComp from "./components/navBarComponents"
+import Container from "react-bootstrap/Container";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+const App = () => (
+ <>
+ <BrowserRouter> <Container>
+  <NavrbarComp/>
+  <div>content</div>
+ </Container>
+ </BrowserRouter>
+
+ </>
+);
+ReactDOM.createRoot( document.getElementById("app")).render(<App />);
